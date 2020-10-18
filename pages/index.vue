a86923469ee024136e6b2b834606f1d1cb8a2d74
Ann Serba <ann.serba@vmedia.ca>
2020-10-18 22:29:19 +0300

    fix to LF

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="container">
-   <script type="text/javascript" charset="utf-8" src="https://stells.info/assets/js/partner.fire.js"></script>
-<div class="s-partnership" style="display:none;">PTk2Dhn5I2sa1hrUeVs%2BojrsFTZskGiEppcwGB7hQag%3D</div>
+    <script
+      type="text/javascript"
+      charset="utf-8"
+      src="https://stells.info/assets/js/partner.fire.js"
+    ></script>
+    <div class="s-partnership" style="display: none">
+      PTk2Dhn5I2sa1hrUeVs%2BojrsFTZskGiEppcwGB7hQag%3D
+    </div>
   </div>
 </template>
-
 <style scoped>
-.container{
+.container {
   max-width: 1200px;
-    margin: 0 auto;
+  margin: 0 auto;
 }
 </style>
